8e461bbcb3d2b1a3d267c781de80e6e4d5978cb3
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 17:44:25 +0100

    Voeg setup voor collector account toe

--- admin/create_test_users.php
+++ admin/create_test_users.php
@@ -5,3 +5,8 @@ $adminEmail = "admin@test.nl";
 $adminPassword = password_hash("admin123", PASSWORD_BCRYPT);
 $adminRole = "admin";
 $adminBlocked = 0;
+
+$collectorEmail = "collector@test.nl";
+$collectorPassword = password_hash("collector123", PASSWORD_BCRYPT);
+$collectorRole = "collector";
+$collectorBlocked = 0;
