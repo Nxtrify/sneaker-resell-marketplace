cfd5f680784e0ce5ca9113dbb2cb42917c8189a6
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 17:04:01 +0100

    Voeg ophalen van sneakers voor verificatie toe

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,3 +6,15 @@ requireAdmin();
 
 <h2>Admin Dashboard</h2>
 <p>Welkom Admin</p>
+
+<?php
+require_once "../config/database.php";
+require_once "../includes/auth.php";
+requireLogin();
+requireAdmin();
+
+$stmt = $pdo->query("SELECT s.*, u.email as seller_email FROM sneakers s 
+                     JOIN users u ON s.user_id=u.id 
+                     WHERE s.status='verification'");
+$sneakers = $stmt->fetchAll();
+?>
