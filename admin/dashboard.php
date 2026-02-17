13991bede70c128947032262190d40aef0bb1786
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 19:14:32 +0100

    Correctie

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,11 +1,9 @@
-<p><a href="../public/logout.php">Uitloggen</a></p>
-
 <?php
 require_once "../includes/auth.php";
 requireLogin();
 requireAdmin();
 ?>
-
+<p><a href="../public/logout.php">Uitloggen</a></p>
 <h2>Admin Dashboard</h2>
 <p>Welkom Admin</p>
 
