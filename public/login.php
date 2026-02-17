9018aa363f6d38afde60d90965d0e893b67e96fa
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 15:56:32 +0100

    Gebruiker ophalen bij login

---
+++ public/login.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../config/database.php";
+require_once "../includes/auth.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch();
