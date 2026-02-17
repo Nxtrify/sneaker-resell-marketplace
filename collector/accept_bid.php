d49876271c3776ad3412f47ef6dbb887a7fc55c2
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 17:15:20 +0100

    Voeg POST-validatie toe voor bodacceptatie

--- collector/accept_bid.php
+++ collector/accept_bid.php
@@ -3,7 +3,6 @@ require_once "../config/database.php";
 require_once "../includes/auth.php";
 requireLogin();
 
-// Haal data van POST
 $sneaker_id = $_POST['sneaker_id'] ?? null;
 $bid_id = $_POST['bid_id'] ?? null;
 
