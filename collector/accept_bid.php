ac4a0b595291c1a4822c928206790c65096f89fd
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 17:16:36 +0100

    Voeg verificatie toe dat sneaker bij ingelogde gebruiker hoort

--- collector/accept_bid.php
+++ collector/accept_bid.php
@@ -6,6 +6,9 @@ requireLogin();
 $sneaker_id = $_POST['sneaker_id'] ?? null;
 $bid_id = $_POST['bid_id'] ?? null;
 
-if (!$sneaker_id || !$bid_id) {
-    die("Ongeldige data.");
-}
+if (!$sneaker_id || !$bid_id) die("Ongeldige data");
+
+$stmt = $pdo->prepare("SELECT * FROM sneakers WHERE id=? AND user_id=?");
+$stmt->execute([$sneaker_id, $_SESSION['user_id']]);
+$sneaker = $stmt->fetch();
+if (!$sneaker) die("Niet jouw sneaker.");
