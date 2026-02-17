3aae42f3cb27cf5c1547724c59b95134d8c9d3bd
Ayman Gaddani <2153432@talnet.nl>
2026-02-17 15:57:42 +0100

    Voeg inlogcontrole en doorverwijzing toe

--- public/login.php
+++ public/login.php
@@ -9,3 +9,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch();
+
+     if ($user && password_verify($password, $user["password"])) {
+        if ($user["blocked"] == 1) {
+            $error = "Account is geblokkeerd.";
+        } else {
+            $_SESSION["user_id"] = $user["id"];
+            $_SESSION["role"] = $user["role"];
+            if ($user["role"] === "admin") {
+                header("Location: ../admin/dashboard.php");
+            } else {
+                header("Location: ../collector/dashboard.php");
+            }
+            exit();
+        }
+    } else {
+        $error = "Onjuiste gegevens.";
+    }
+}
+?>
